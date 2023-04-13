80020aca9c14dee9f2bee06be482786adc0a6402
Eldin Kecap <eldinkecap@gmail.com>
2023-04-13 23:51:41 +0200

    added some more functionality to the spin boxes

--- dialog.cpp
+++ dialog.cpp
@@ -6,11 +6,58 @@ Dialog::Dialog(QWidget *parent) :
     ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+    prioritetSpinBoxes[0] = ui->prioritetP1;
+    prioritetSpinBoxes[1] = ui->prioritetP2;
+    prioritetSpinBoxes[2] = ui->prioritetP3;
+    prioritetSpinBoxes[3] = ui->prioritetP4;
+    prioritetSpinBoxes[4] = ui->prioritetP5;
+    prioritetSpinBoxes[5] = ui->prioritetP6;
+    prioritetSpinBoxes[6] = ui->prioritetP7;
+
+    brojCiklusaSpinBoxes[0] = ui->brojCiklusaP1;
+    brojCiklusaSpinBoxes[1] = ui->brojCiklusaP2;
+    brojCiklusaSpinBoxes[2] = ui->brojCiklusaP3;
+    brojCiklusaSpinBoxes[3] = ui->brojCiklusaP4;
+    brojCiklusaSpinBoxes[4] = ui->brojCiklusaP5;
+    brojCiklusaSpinBoxes[5] = ui->brojCiklusaP6;
+    brojCiklusaSpinBoxes[6] = ui->brojCiklusaP7;
+
+    dolazakSpinBoxes[0] = ui->dolazakP1;
+    dolazakSpinBoxes[1] = ui->dolazakP2;
+    dolazakSpinBoxes[2] = ui->dolazakP3;
+    dolazakSpinBoxes[3] = ui->dolazakP4;
+    dolazakSpinBoxes[4] = ui->dolazakP5;
+    dolazakSpinBoxes[5] = ui->dolazakP6;
+    dolazakSpinBoxes[6] = ui->dolazakP7;
+
+
+
 }
 
 Dialog::~Dialog()
 {
     delete ui;
+}
+
+void Dialog::disableSpinBoxesBasedOnSelectedAmount(int numOfProcesses)
+{
+
+    for(int i = 0 ; i < numOfProcesses; i++ ){
+        brojCiklusaSpinBoxes[i]->setEnabled(true);
+        dolazakSpinBoxes[i]->setEnabled(true);
+        if(ui->algoritam->currentText() == "Prioritet"){
+            prioritetSpinBoxes[i]->setEnabled(true);
+        }
+    }
+
+    for(int i = numOfProcesses ; i < 7; i++ ){
+        brojCiklusaSpinBoxes[i]->setEnabled(false);
+        dolazakSpinBoxes[i]->setEnabled(false);
+        if(ui->algoritam->currentText() == "Prioritet"){
+            prioritetSpinBoxes[i]->setEnabled(false);
+        }
+    }
+
 }
 
 void Dialog::on_pushButton_clicked()
@@ -19,4 +66,24 @@ void Dialog::on_pushButton_clicked()
     qDebug()<<algoritam;
     QString brojProcesa = ui->brojProcesa->currentText();
     qDebug()<<brojProcesa;
+
+
+
+}
+
+void Dialog::on_brojProcesa_activated(const QString &brojProcesa)
+{
+    disableSpinBoxesBasedOnSelectedAmount(brojProcesa.toInt());
+}
+
+
+void Dialog::on_algoritam_activated(const QString &arg1)
+{
+    if(arg1 == "Prioritet"){
+        disableSpinBoxesBasedOnSelectedAmount(ui->brojProcesa->currentText().toInt());
+    }else if(arg1 != "Prioritet"){
+        for(int i = 0; i < 7 ; i++){
+            prioritetSpinBoxes[i]->setEnabled(false);
+        }
+    }
 }
--- dialog.h
+++ dialog.h
@@ -4,6 +4,9 @@
 #include <QDialog>
 #include <QString>
 #include <QDebug>
+#include <QVector>
+#include <QSpinBox>
+#include <QGraphicsScene>
 
 namespace Ui {
 class Dialog;
@@ -16,12 +19,21 @@ class Dialog : public QDialog
 public:
     explicit Dialog(QWidget *parent = 0);
     ~Dialog();
+    void disableSpinBoxesBasedOnSelectedAmount(int numOfProcesses);
 
 private slots:
     void on_pushButton_clicked();
 
+    void on_brojProcesa_activated(const QString &arg1);
+
+    void on_algoritam_activated(const QString &arg1);
+
 private:
     Ui::Dialog *ui;
+    QGraphicsScene *scene;
+    QSpinBox* prioritetSpinBoxes[7];
+    QSpinBox* brojCiklusaSpinBoxes[7];
+    QSpinBox* dolazakSpinBoxes[7];
 };
 
 #endif // DIALOG_H
